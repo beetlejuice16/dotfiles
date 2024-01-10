618c76383a7dedd1b5488ba70a43ceae36d00c21
Fayez <fayez.bajjali@proton.me>
2024-01-10 09:04:08 +0100

    Delete autofuncs replaced by folke/trouble

--- dot_config/nvim/lua/beetle/autofuncs.lua
+++
@@ -1,6 +0,0 @@
-local group = vim.api.nvim_create_augroup("ShowErrorsOnSave", { clear = true })
-
-vim.api.nvim_create_autocmd('BufWritePost', {
-    command = "lua vim.diagnostic.setloclist({ severity = vim.diagnostic.severity.ERROR} )",
-    group = group
-})
